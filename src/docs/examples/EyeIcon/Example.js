183ad0422354d146d689ec35df9e887cdb62795e
Nicolas Gordillo <nicogordillo@gmail.com>
2020-03-09 12:31:39 -0300

    Atom components: Label, ProgressBar and EyeIcon

---
+++ src/docs/examples/EyeIcon/Example.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import EyeIcon from 'ps-react-ng/EyeIcon';
+
+/** TODO: For icons, consider writing a script to generate a static page of icons. Then weave that into your doc generation */
+export default function Example() {
+    return <EyeIcon />;
+}
