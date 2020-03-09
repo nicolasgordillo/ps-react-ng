8f091cc4f47ce66e218a247311babf99adf8bca5
Nicolas Gordillo <nicogordillo@gmail.com>
2020-03-09 00:50:34 -0300

    Added code examples

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/** Component that shows a message on screen */
 function HelloWorld({ message} ) {
     return <div>Hello { message }!</div>
 }
 
 HelloWorld.propTypes = {
+    /** Message to display */
     message: PropTypes.string
+}
+
+HelloWorld.defaultProps = {
+    message: 'World'
 }
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Docs from './docs/Docs';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/highlight.js/styles/ocean.css';
 
 ReactDOM.render(<Docs />, document.getElementById('root'));
 
